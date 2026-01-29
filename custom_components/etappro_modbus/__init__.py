0e09a30f66104fef9ccd8d9d7d80edc02ad05ceb
Straybiker <stray@telenet.be>
2026-01-29 17:55:54 +0100

    fix reading device properties

--- custom_components/etappro_modbus/__init__.py
+++ custom_components/etappro_modbus/__init__.py
@@ -86,6 +86,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Register the hub."""
     hass.data[DOMAIN][name] = {"hub": hub}
 
+    # Read device info before setting up platforms so device_info is available
+    hub.connect()
+    await hub.read_modbus_data()
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
